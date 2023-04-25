66c94560876f9f2b4fad2114cb74b0836e6d0941
xiaoyongsheng <xiaoyongsheng@perfee.com>
2023-04-25 15:46:39 +0800

    update: unified_billing_cycle

--- common/unified_billing_cycle.py
+++ common/unified_billing_cycle.py
@@ -7,7 +7,7 @@ from datetime import datetime, timedelta
 
 
 K_DB_URL = {
-    "dev": "mongodb://root:KB5NF1T0aP@mongodb-headless.os:27017/admin?replicaSet=rs0",
+    "dev": "mongodb://root:NX9NfRBkth@mo-mongodb-headless.os.svc/admin?replicaSet=rs0",
 	"test": "mongodb://root:IdrCgVpHzv@mongo-mongodb-headless.os:27017/admin?replicaSet=rs0&retrywrites=false",
     # "prd": "mongodb://lite-prd",
 }
@@ -47,18 +47,64 @@ def record_settlement_cycle():
         else:
             settlement_cycle = store["settlementAccount"]["settlementCycle"]
         no = store.get("qualification", {}).get("idNumber")
-        if no:
-            res = common_db.UnifiedBillingCycle.find_one(
+        if no and no not in ["null", None]:
+            b_cycle = common_db.UnifiedBillingCycle.find_one(
                 {"businessLicenseNo": no})
-            if res and res["settlementCycle"] != settlement_cycle:
-                seller_db.Store.update_one(
-                    {"_id": store["_id"]},
-                    {"$set": {"settlementAccount.settlementCycle":
-                                  res["settlementCycle"]}})
+            if b_cycle:
+                if b_cycle["settlementCycle"] in [3, 4]:
+                    update_cycle = 2
+                else:
+                    update_cycle = b_cycle["settlementCycle"]
+
+                if update_cycle != settlement_cycle:
+                    seller_db.Store.update_one(
+                        {"_id": store["_id"]},
+                        {"$set": {"settlementAccount.settlementCycle":
+                                      update_cycle}})
             else:
                 common_db.UnifiedBillingCycle.insert_one(
                     {"businessLicenseNo": no,
                      "settlementCycle": settlement_cycle})
+        elif store.get("kiliPartnerInfo"):
+            seller_db.Store.update_one(
+                {"_id": store["_id"]},
+                {"$set": {"qualification.idNumber": store.get(
+                    "kiliPartnerInfo").get("kiliUserId")}})
+
+    for vendor in quark_vendor_db.Vendor.find():
+        if vendor.get("businessLicenseNo"):
+            common_db.UnifiedBillingCycle.find_one(
+                {"businessLicenseNo": vendor.get("businessLicenseNo")})
+
+    lite_type = bee_auth_db.UserType.find_one(
+        {"platformName": "Kilimall Lite"})["_id"]  # PerFee
+    supply_type = bee_auth_db.UserType.find_one(
+        {"platformName": "Uomnify"})["_id"]
+    print(lite_type, supply_type)
+    for store in bee_auth_db.Store.find():
+        if store.get("userTypeId") == lite_type:
+            st = seller_db.Store.find_one(
+                {"_id": int(store["platformStoreId"])})
+            if st and st.get("qualification"):
+                id_num = st["qualification"].get("idNumber")
+                if not id_num:
+                    id_num = st.get("kiliPartnerInfo", {}).get("kiliUserId")
+                bee_auth_db.CustomerInfo.find_one_and_update(
+                    {"userId": store["userId"]},
+                    {"$set": {
+                        "idNum": id_num,
+                        "settlementCycle": st.get("settlementAccount", {}).get(
+                            "settlementCycle")}}, upsert=True)
+        elif store.get("userTypeId") == supply_type:
+            vd = quark_vendor_db.Vendor.find_one(
+                {"_id": int(store["platformStoreId"])})
+            if vd:
+                cycle = vd["settlementMethod"]
+                if cycle in [3, 4]:
+                    cycle = 2
+                bee_auth_db.CustomerInfo.find_one_and_update(
+                    {"userId": store["userId"]},
+                    {"$set": {"settlementCycle": cycle}}, upsert=True)
 
 
 if __name__ == "__main__":
@@ -73,6 +119,7 @@ if __name__ == "__main__":
     bee_common_db = get_db(K_DB_URL, env, "BeeCommon")
     common_db = get_db(K_DB_URL, env, "Common")
     seller_db = get_db(K_DB_URL, env, "Seller")
-    quark_common_db = get_db(K_DB_URL, env, "QuarkCommon")
+    quark_vendor_db = get_db(K_DB_URL, env, "QuarkVendor")
+    bee_auth_db = get_db(K_DB_URL, env, "BeeAuth")
 
     record_settlement_cycle()
